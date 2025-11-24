2617ffe9e6d863cccfbff842fa034c6967a1956c
Onimuxha <cheathekm@gmail.com>
2025-11-24 17:02:26 +0700

    fix bug

--- client/public/sw.js
+++ client/public/sw.js
@@ -6,7 +6,7 @@ const urlsToCache = [
   '/favicon.png',
   '/static/js/bundle.js',
   '/static/css/main.css',
-  '/InterKhmerLooped.ttf',``
+  '/InterKhmerLooped.ttf',
 ];
 
 self.addEventListener('install', (event) => {
@@ -66,56 +66,3 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
-
-
-// const CACHE_NAME = 'weekly-schedule-v1';
-// const urlsToCache = [
-//   '/',
-//   '/index.html',
-//   '/manifest.json',
-//   '/favicon.png',
-// ];
-
-// self.addEventListener('install', (event) => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => {
-//       return cache.addAll(urlsToCache);
-//     })
-//   );
-//   self.skipWaiting();
-// });
-
-// self.addEventListener('activate', (event) => {
-//   event.waitUntil(
-//     caches.keys().then((cacheNames) => {
-//       return Promise.all(
-//         cacheNames.map((cacheName) => {
-//           if (cacheName !== CACHE_NAME) {
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-//   self.clients.claim();
-// });
-
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((response) => {
-//       if (response) {
-//         return response;
-//       }
-//       return fetch(event.request).then((response) => {
-//         if (!response || response.status !== 200 || response.type !== 'basic') {
-//           return response;
-//         }
-//         const responseToCache = response.clone();
-//         caches.open(CACHE_NAME).then((cache) => {
-//           cache.put(event.request, responseToCache);
-//         });
-//         return response;
-//       });
-//     })
-//   );
-// });
